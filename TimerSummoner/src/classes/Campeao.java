c04702705ba780f8e6cee7d1b6cc340d91b25b2d
Alexandre Ferreira <alexandre.mem.ferreira@gmail.com>
2018-05-30 00:47:36 -0300

    Classes iniciais

---
+++ TimerSummoner/src/classes/Campeao.java
@@ -0,0 +1,23 @@
+package classes;
+
+public class Campeao {
+	private String nome;
+	private String apelido;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+}
---
+++ TimerSummoner/src/classes/Spells.java
@@ -0,0 +1,23 @@
+package classes;
+
+public class Spells {
+	private String nome;
+	private Integer tempo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getTempo() {
+		return tempo;
+	}
+
+	public void setTempo(Integer tempo) {
+		this.tempo = tempo;
+	}
+
+}
